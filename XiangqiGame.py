090f8b4bf40433cbb6a0fd38082ac36186600def
Daniel Gwon <gwond@oregonstate.edu>
2020-03-01 20:40:27 -0500

    uploading some files

--- XiangqiGame.py
+++ XiangqiGame.py
@@ -43,38 +43,38 @@ class Board:
         self._board = [['' for _ in range(9)] for _ in range(10)]   # 10 rows x 9 cols
 
         # set up the board
-        self._board[0][4] = gen_b = General('black', 'e1')
-        self._board[0][3] = adv_b_1 = Advisor('black', 'd1')
-        self._board[0][5] = adv_b_2 = Advisor('black', 'f1')
-        self._board[0][2] = ele_b_1 = Elephant('black', 'c1')
-        self._board[0][6] = ele_b_2 = Elephant('black', 'g1')
-        self._board[0][1] = hor_b_1 = Horse('black', 'b1')
-        self._board[0][7] = hor_b_2 = Horse('black', 'h1')
-        self._board[0][0] = cha_b_1 = Chariot('black', 'a1')
-        self._board[0][8] = cha_b_2 = Chariot('black', 'i1')
-        self._board[2][1] = can_b_1 = Cannon('black', 'b3')
-        self._board[2][7] = can_b_2 = Cannon('black', 'h3')
-        self._board[3][0] = sol_b_1 = Soldier('black', 'a4')
-        self._board[3][2] = sol_b_2 = Soldier('black', 'c4')
-        self._board[3][4] = sol_b_3 = Soldier('black', 'e4')
-        self._board[3][6] = sol_b_4 = Soldier('black', 'g4')
-        self._board[3][8] = sol_b_5 = Soldier('black', 'i4')
-        self._board[9][4] = gen_r = General('red', 'e10')
-        self._board[9][3] = adv_r_1 = Advisor('red', 'd10')
-        self._board[9][5] = adv_r_2 = Advisor('red', 'f10')
-        self._board[9][2] = ele_r_1 = Elephant('red', 'c10')
-        self._board[9][6] = ele_r_2 = Elephant('red', 'g10')
-        self._board[9][1] = hor_r_1 = Horse('red', 'b10')
-        self._board[9][7] = hor_r_2 = Horse('red', 'h10')
-        self._board[9][0] = cha_r_1 = Chariot('red', 'a10')
-        self._board[9][8] = cha_r_2 = Chariot('red', 'i10')
-        self._board[7][1] = can_r_1 = Cannon('red', 'b8')
-        self._board[7][7] = can_r_2 = Cannon('red', 'h8')
-        self._board[6][0] = sol_r_1 = Soldier('red', 'a7')
-        self._board[6][2] = sol_r_2 = Soldier('red', 'c7')
-        self._board[6][4] = sol_r_3 = Soldier('red', 'e7')
-        self._board[6][6] = sol_r_4 = Soldier('red', 'g7')
-        self._board[6][8] = sol_r_5 = Soldier('red', 'i7')
+        self._board[0][4] = gen_b = General('black', 0, 4)
+        self._board[0][3] = adv_b_1 = Advisor('black', 0, 3)
+        self._board[0][5] = adv_b_2 = Advisor('black', 0, 5)
+        self._board[0][2] = ele_b_1 = Elephant('black', 0, 2)
+        self._board[0][6] = ele_b_2 = Elephant('black', 0, 6)
+        self._board[0][1] = hor_b_1 = Horse('black', 0, 1)
+        self._board[0][7] = hor_b_2 = Horse('black', 0, 7)
+        self._board[0][0] = cha_b_1 = Chariot('black', 0, 0)
+        self._board[0][8] = cha_b_2 = Chariot('black', 0, 8)
+        self._board[2][1] = can_b_1 = Cannon('black', 2, 1)
+        self._board[2][7] = can_b_2 = Cannon('black', 2, 7)
+        self._board[3][0] = sol_b_1 = Soldier('black', 3, 0)
+        self._board[3][2] = sol_b_2 = Soldier('black', 3, 2)
+        self._board[3][4] = sol_b_3 = Soldier('black', 3, 4)
+        self._board[3][6] = sol_b_4 = Soldier('black', 3, 6)
+        self._board[3][8] = sol_b_5 = Soldier('black', 3, 8)
+        self._board[9][4] = gen_r = General('red', 9, 4)
+        self._board[9][3] = adv_r_1 = Advisor('red', 9, 3)
+        self._board[9][5] = adv_r_2 = Advisor('red', 9, 5)
+        self._board[9][2] = ele_r_1 = Elephant('red', 9, 2)
+        self._board[9][6] = ele_r_2 = Elephant('red', 9, 6)
+        self._board[9][1] = hor_r_1 = Horse('red', 9, 1)
+        self._board[9][7] = hor_r_2 = Horse('red', 9, 7)
+        self._board[9][0] = cha_r_1 = Chariot('red', 9, 0)
+        self._board[9][8] = cha_r_2 = Chariot('red', 9, 8)
+        self._board[7][1] = can_r_1 = Cannon('red', 7, 1)
+        self._board[7][7] = can_r_2 = Cannon('red', 7, 7)
+        self._board[6][0] = sol_r_1 = Soldier('red', 6, 0)
+        self._board[6][2] = sol_r_2 = Soldier('red', 6, 2)
+        self._board[6][4] = sol_r_3 = Soldier('red', 6, 4)
+        self._board[6][6] = sol_r_4 = Soldier('red', 6, 6)
+        self._board[6][8] = sol_r_5 = Soldier('red', 6, 8)
 
     def update_board(self):
         pass
@@ -89,10 +89,11 @@ class Board:
 class Piece:
     """"""
 
-    def __init__(self, player, pos):
+    def __init__(self, player, r, c):
         """"""
         self._player = player
-        self._pos = pos
+        self._row = r
+        self._col = c
 
     def get_player(self):
         return self._player
@@ -107,10 +108,12 @@ class Piece:
 class General(Piece):
     """"""
 
-    def __init__(self, player, pos):
+    def __init__(self, player, r, c):
         """"""
+        super().__init__(player, r, c)
         self._player = player
-        self._pos = pos
+        self._row = r
+        self._col = c
 
     def make_move(self, new_pos):
         """"""
@@ -122,10 +125,12 @@ class General(Piece):
 class Advisor(Piece):
     """"""
 
-    def __init__(self, player, pos):
+    def __init__(self, player, r, c):
         """"""
+        super().__init__(player, r, c)
         self._player = player
-        self._pos = pos
+        self._row = r
+        self._col = c
 
     def make_move(self, new_pos):
         """"""
@@ -135,10 +140,12 @@ class Advisor(Piece):
 class Elephant(Piece):
     """"""
 
-    def __init__(self, player, pos):
+    def __init__(self, player, r, c):
         """"""
+        super().__init__(player, r, c)
         self._player = player
-        self._pos = pos
+        self._row = r
+        self._col = c
 
     def make_move(self, new_pos):
         """"""
@@ -148,10 +155,12 @@ class Elephant(Piece):
 class Horse(Piece):
     """"""
 
-    def __init__(self, player, pos):
+    def __init__(self, player, r, c):
         """"""
+        super().__init__(player, r, c)
         self._player = player
-        self._pos = pos
+        self._row = r
+        self._col = c
 
     def make_move(self, new_pos):
         """"""
@@ -161,10 +170,12 @@ class Horse(Piece):
 class Chariot(Piece):
     """"""
 
-    def __init__(self, player, pos):
+    def __init__(self, player, r, c):
         """"""
+        super().__init__(player, r, c)
         self._player = player
-        self._pos = pos
+        self._row = r
+        self._col = c
 
     def make_move(self, new_pos):
         """"""
@@ -174,10 +185,12 @@ class Chariot(Piece):
 class Cannon(Piece):
     """"""
 
-    def __init__(self, player, pos):
+    def __init__(self, player, r, c):
         """"""
+        super().__init__(player, r, c)
         self._player = player
-        self._pos = pos
+        self._row = r
+        self._col = c
 
     def make_move(self, new_pos):
         """"""
@@ -187,37 +200,17 @@ class Cannon(Piece):
 class Soldier(Piece):
     """"""
 
-    def __init__(self, player, pos):
+    def __init__(self, player, r, c):
         """"""
+        super().__init__(player, r, c)
         self._player = player
-        self._pos = pos
+        self._row = r
+        self._col = c
 
-    def make_move(self, new_pos):
+    def make_move(self, r, c):
         """"""
-        # TODO check if move is valid - see General
-
-
-# General - one point orthogonal, in castle only
-# Advisor - one point diagonal, in castle only
-# Elephant - two points diagonal, can't cross river
-# Horse - one point orthogonal and one point diagonal, can be blocked
-# Chariot - to any point orthogonally
-# Cannon - to any point orthogonally, needs screen to capture
-# Soldier - one point forward, never back; also horizontal after crossing river
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if self._player == 'black':
+            if r < 5:
+                if self._row
 
 
