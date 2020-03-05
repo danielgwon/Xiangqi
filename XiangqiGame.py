6dbae44a2e5f61b53cc3e1e34b1cb7911ca3ba8d
gwond <gwond@oregonstate.edu>
2020-03-04 19:04:07 -0500

    more updates

--- XiangqiGame.py
+++ XiangqiGame.py
@@ -4,29 +4,50 @@
 
 
 class XiangqiGame:
-    """"""
+    """
+    Creates a game of Xiangqi
+    """
 
     def __init__(self):
-        """"""
+        """
+        init the game
+        """
         self._game_state = 'UNFINISHED'         # 'UNFINISHED', 'RED_WON', 'BLACK_WON'
-        self._red_in_check = ''
-        self._black_in_check = ''
-        self._turn = 'red'
+        self._red_in_check = None
+        self._black_in_check = None
+        self._turn = 'red'                      # red starts
         self._board = Board()
 
     def get_game_state(self):
-        """"""
+        """
+        gives the current game state
+        :return: str ('UNFINISHED', 'RED_WON', or 'BLACK_WON')
+        """
         return self._game_state
+
+    def set_game_state(self, state):
+        """
+        update the game state
+        :param state: str
+        :return: n/a
+        """
+        self._game_state = state
 
     def is_in_check(self, player):
         """
-
-        :param player: 'red' or 'black'
-        :return: True or False
+        tells if given player is in check
+        :param player: str ('red' or 'black')
+        :return: bool
         """
+        if player == 'red':
+            return self._red_in_check
+        return self._black_in_check
 
     def update_turn(self):
-        """"""
+        """
+        updates whose turn it is
+        :return: n/a
+        """
         if self._turn == 'red':
             self._turn = 'black'
         else:
@@ -34,13 +55,19 @@ class XiangqiGame:
 
     def make_move(self, start, to):
         """
-        Moves a piece from a start position to a to position
+        moves a piece from start position to to position
         :param start: str
         :param to: str
         :return: bool or n/a
         """
 
+        # check for following here:
+        #       move is in bounds
+        #       move has a piece
+        #       move has a piece belonging to correct player
+
         # convert start to board indices
+        # TODO convert method?
         row_s = int(start[1])
         col_s = int(start[0])
         row_t = int(to[1])
@@ -74,10 +101,16 @@ class XiangqiGame:
 
 
 class Board:
-    """a Board object that holds pieces of the game"""
+    """
+    Creates a Xiangqi board
+    """
 
     def __init__(self):
-        """"""
+        """
+        init the board
+        """
+
+        # initial board
         self._board = [['' for _ in range(9)] for _ in range(10)]   # 10 rows x 9 cols
 
         # set up the board
@@ -115,107 +148,232 @@ class Board:
         self._board[6][8] = sol_r_5 = Soldier('red', 6, 8)
 
     def make_move(self, row_s, col_s, row_t, col_t):
-        """"""
+        """
+        takes piece in the _s position to the _t position
+        :param row_s: int
+        :param col_s: int
+        :param row_t: int
+        :param col_t: int
+        :return: n/a
+        """
 
-        result = self._board[row_s][col_s].make_move(row_t, col_t)
-
-        if not result:
-            return False
+        # XiangqiGame will check if move has piece, and if piece belongs to correct player
+        self._board[row_s][col_s].make_move(row_t, col_t)
 
     def get_piece(self, r, c):
-        """"""
+        """
+        gives the piece at the specified row and col
+        :param r: int
+        :param c: int
+        :return: '' or Piece
+        """
         return self._board[r][c]
 
     def get_board(self):
-        pass
+        """
+        gives the board
+        :return: [[]]
+        """
+        return self._board
 
     def print_board(self):
-        """"""
+        """
+        prints the board
+        :return: n/a
+        """
         for i in range(len(self._board)-1, -1, -1):
             print(self._board[i])
 
 
 class Piece:
-    """"""
+    """
+    Creates a Xiangqi piece on the board
+    """
 
     def __init__(self, player, r, c):
-        """"""
+        """
+        init the Piece
+        :param player: str ('red' or 'black')
+        :param r: int
+        :param c: int
+        """
         self._player = player
         self._row = r
         self._col = c
 
     def get_player(self):
+        """
+        gives the owner of the Piece
+        :return: str ('red' or 'black')
+        """
         return self._player
 
     def get_row(self):
+        """
+        gives the row of the Piece
+        :return: int
+        """
         return self._row
 
     def get_col(self):
+        """
+        gives the col of the Piece
+        :return: int
+        """
         return self._col
 
 
 class General(Piece):
-    """"""
+    """
+    Creates a General Piece
+    """
 
     def __init__(self, player, r, c):
-        """"""
-        super().__init__(player, r, c)
+        """
+        init the General
+        :param player: str ('red' or 'black')
+        :param r: int
+        :param c: int
+        """
 
-    def make_move(self, new_pos):
-        """"""
-        # TODO check if move is valid
-        # if new_pos is valid, self.set_pos(new_pos)
-        # else return False
-
-
-class Advisor(Piece):
-    """"""
-
-    def __init__(self, player, r, c):
-        """"""
-        super().__init__(player, r, c)
-
-    def make_move(self, new_pos):
-        """"""
-        # TODO check if move is valid - see General
-
-
-class Elephant(Piece):
-    """"""
-
-    def __init__(self, player, r, c):
-        """"""
-        super().__init__(player, r, c)
-
-    def make_move(self, new_pos):
-        """"""
-        # TODO check if move is valid - see General
-
-
-class Horse(Piece):
-    """"""
-
-    def __init__(self, player, r, c):
-        """"""
-        super().__init__(player, r, c)
-
-    def make_move(self, new_pos):
-        """"""
-        # TODO check if move is valid - see General
-
-
-class Chariot(Piece):
-    """"""
-
-    def __init__(self, player, r, c):
-        """"""
+        # inherits from Piece
         super().__init__(player, r, c)
 
     def make_move(self, r, c):
-        """"""
+        """
+        moves the General to the given position
+        :param r: int
+        :param c: int
+        :return:
+        """
+        # TODO check if move is valid
+        # 1. no across from other General
+        # 2. single space orthogonally
+        # 3. castle only
+        # 4. not in check following move
+        # else return False
+        # is_valid method?
+
+
+class Advisor(Piece):
+    """
+    Creates an Advisor Piece
+    """
+
+    def __init__(self, player, r, c):
+        """
+        init the Advisor
+        :param player: str ('red' or 'black')
+        :param r: int
+        :param c: int
+        """
+
+        # inherits from Piece
+        super().__init__(player, r, c)
+
+    def make_move(self, r, c):
+        """
+        moves the Advisor to the given position
+        :param r: int
+        :param c: int
+        :return:
+        """
+        # TODO check if move is valid
+        # 1. single space diagonal
+        # 2. castle only
+        # is_valid method?
+
+
+
+class Elephant(Piece):
+    """
+    Creates an Elephant Piece
+    """
+
+    def __init__(self, player, r, c):
+        """
+        init the Elephant
+        :param player: str ('red' or 'black')
+        :param r: int
+        :param c: int
+        """
+
+        # inherit from Piece
+        super().__init__(player, r, c)
+
+    def make_move(self, r, c):
+        """
+        moves the Elephant to the given position
+        :param r: int
+        :param c: int
+        :return:
+        """
+        # TODO check if move is valid
+        # 1. two points diagonally
+        # 2. can't cross river
+        # is_valid method?
+
+
+class Horse(Piece):
+    """
+    Creates a Horse Piece
+    """
+
+    def __init__(self, player, r, c):
+        """
+        init the Horse
+        :param player: str ('red' or 'black')
+        :param r: int
+        :param c: int
+        """
+
+        # inherit from Piece
+        super().__init__(player, r, c)
+
+    def make_move(self, r, c):
+        """
+        moves the Horse to the given position
+        :param r: int
+        :param c: int
+        :return:
+        """
+        # TODO check if move is valid
+        # 1. one point orthogonally, one point diagonally
+        # 2. can be blocked
+        # is_valid method?
+
+
+class Chariot(Piece):
+    """
+    Creates a Chariot Piece
+    """
+
+    def __init__(self, player, r, c):
+        """
+        init the Chariot
+        :param player: str ('red' or 'black')
+        :param r: int
+        :param c: int
+        """
+
+        # inherit from Piece
+        super().__init__(player, r, c)
+
+    def make_move(self, r, c):
+        """
+        moves the Chariot to the given position
+        :param r: int
+        :param c: int
+        :return:
+        """
+
+        # TODO check if move is valid
+        # 1. any point orthogonally
+        # is_valid method?
+
         if r == self._row and c > self._col:        # move right
-            for (i in range(self._col, c)):
-                if
+            for i in range(self._col, c):
+
         elif r == self._row and c < self._col:      # move left
 
         elif r < self._row and c == self._col:      # move forward
@@ -225,26 +383,63 @@ class Chariot(Piece):
 
 
 class Cannon(Piece):
-    """"""
+    """
+    Creates a Cannon Piece
+    """
 
     def __init__(self, player, r, c):
-        """"""
-        super().__init__(player, r, c)
+        """
+        init the Cannon
+        :param player: str ('red' or 'black')
+        :param r: int
+        :param c: int
+        """
 
-    def make_move(self, new_pos):
-        """"""
-        # TODO check if move is valid - see General
-
-
-class Soldier(Piece):
-    """"""
-
-    def __init__(self, player, r, c):
-        """"""
+        # inherit from Piece
         super().__init__(player, r, c)
 
     def make_move(self, r, c):
-        """"""
+        """
+        moves the Cannon to the given position
+        :param r: int
+        :param c: int
+        :return:
+        """
+        # TODO check if move is valid
+        # 1. any point orthogonally
+        # 2. needs screen to capture
+
+
+class Soldier(Piece):
+    """
+    Creates a Soldier Piece
+    """
+
+    def __init__(self, player, r, c):
+        """
+        init the Soldier
+        :param player: str ('red' or 'black')
+        :param r: int
+        :param c: int
+        """
+
+        # inherit from Piece
+        super().__init__(player, r, c)
+
+    def make_move(self, r, c):
+        """
+        moves the Soldier to the given position
+        :param r: int
+        :param c: int
+        :return:
+        """
+
+        # TODO check if move is valid
+        # 1. one point forward
+        # 2. one point horizontal after crossing river
+        # 4. never back
+        # is_valid method?
+
         if self._player == 'black':
             if r > 4:                   # before crossing river
                 if r == self._row - 1:  # vertical move
@@ -271,5 +466,3 @@ class Soldier(Piece):
                     self._col = c
                 else:
                     return False
-
-hello
